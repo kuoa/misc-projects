038417003957f5f5a525b075b53c4cbcea1b1a8a
kuoa <postarusandu@gmail.com>
2015-06-19 01:48:29 +0900

    Modified Ca library, renamed read_input[_l] | read_line[_l]. Added read_line_a.

--- Ocaml/ca.ml
+++ Ocaml/ca.ml
@@ -1,9 +1,9 @@
-let read_input file =
+let read_input_l file =
   (** 'a list list of input *)
+  (*old versian was read_input*)
 
   let rez = ref [] in
   let ic = open_in file in
-  Printf.printf "\n";
   
   try
     while true do
@@ -21,6 +21,38 @@ let read_input file =
 
     List.rev !rez
 ;;
+
+let read_line_l file =
+  (** 'a list of input *)
+
+  let rez = ref [] in
+  let ic = open_in file in
+
+  try
+    let line = input_line ic in
+    (* tokenizing the line *)
+    
+    let sList = Str.split (Str.regexp_string " ") line in
+    rez := sList;
+    
+    !rez
+   
+  with End_of_file ->
+    close_in ic;
+
+    !rez
+;;
+
+let read_line_a file =
+  (** 'a array of input *)
+  let s_list = read_line_l file in
+  let size = List.length s_list in
+  let s_array = Array.make size "" in
+  List.iteri (fun i x -> s_array.(i) <- x) s_list;
+
+  s_array
+;;
+  
 
 
 let print_list f list =
--- Ocaml/ca.mli
+++ Ocaml/ca.mli
@@ -1,8 +1,16 @@
-(** read each line and return a float list list *)
-val read_input : string -> string list list
+(** read each line and return a string list list *)
+val read_input_l : string -> string list list
+
+(** read first line and return a string list *)
+val read_line_l : string -> string list
+
+(** 'a array of input *)
+val read_line_a : string -> string array
 
 (** print 'a list using f as a printing function *)
 val print_list : ('a -> unit) -> 'a list -> unit
 
 (** print 'a list list using f as a printing function *)
 val print_2xlist : ('a -> unit) -> 'a list list -> unit
+
+
